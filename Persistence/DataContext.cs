9344636e4428064b7abec1e56f8a4c1ed8ff90b5
votero2 <votero2@yahoo.es>
2020-09-30 13:50:57 -0400

    Initial Commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+             builder.Entity<Value>()
+             .HasData(
+                 new Value{Id = 1, Name = "Value101"},
+                 new Value{Id = 2, Name = "Value102"},
+                 new Value{Id = 3, Name = "Value103"}
+             );
+        }
+    }
+}
